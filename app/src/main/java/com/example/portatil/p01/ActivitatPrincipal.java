7a616d3def87f223b60b7f3a300d6708a683c3fd
Asraidk <pipedown_92@hotmail.com>
2016-10-21 19:34:48 +0200

    version 1.2
    añadido toast, i comentarios

--- app/src/main/java/com/example/portatil/p01/ActivitatPrincipal.java
+++ app/src/main/java/com/example/portatil/p01/ActivitatPrincipal.java
@@ -14,40 +14,42 @@ public class ActivitatPrincipal extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_activitat_principal);
-
+        //instanciem
+        //Un boto per poder interectuar de un layout al altre
         Button boton1 = (Button)findViewById(R.id.button);
+        // elements que necesitarem per dur a terme el pas de un activiti a l'altre
         final EditText introduirNom = (EditText)findViewById(R.id.editText);
         final EditText introduirCognom = (EditText)findViewById(R.id.editText2);
         final EditText introduirEdat = (EditText)findViewById(R.id.editText3);
+        //un "boto" que podra marcar l'usuari
         final CheckBox tic = (CheckBox)findViewById(R.id.checkBox);
 
-
+        //si fa click en el boto que em creat
         boton1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                //crem un bundle--> es el nostre transportador
                 Bundle tren = new Bundle();
-
+                //variable per controlar si el estat del check box
                 Boolean marcat = tic.isChecked();
-
-
-
+                //fem que el bundle que em creat agafi les dades dels objectas relacionats en el
+                //layout vinculat previament mintjançant la R i la id
                 tren.putString("Nom",introduirNom.getText().toString());
                 tren.putString("Cognom",introduirCognom.getText().toString());
                 tren.putString("Edat",introduirEdat.getText().toString());
-
+                //Condicio per veure quin  misatge enviarem si el chex box anomenat tic esta marcat
                 if(marcat==true){
                     tren.putString("marcat","Permet enviar les dades");
                 }
                 else{
                     tren.putString("marcat","No permet enviar les dades");
                 }
-
-                Intent parada =new Intent(ActivitatPrincipal.this,ActivitatSecundaria.class);
-                parada.putExtras(tren);
-                startActivity(parada);
-
-
+                //cream un intent que sera el que ens permet fer el tunel entre 1 i altre activity
+                Intent tunel =new Intent(ActivitatPrincipal.this,ActivitatSecundaria.class);
+                //afeguim al intent el que em carregat al bundle
+                tunel.putExtras(tren);
+                //iniciem l'activyti
+                startActivity(tunel);
             }
         });
     }
--- app/src/main/java/com/example/portatil/p01/ActivitatSecundaria.java
+++ app/src/main/java/com/example/portatil/p01/ActivitatSecundaria.java
@@ -3,6 +3,7 @@ package com.example.portatil.p01;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class ActivitatSecundaria extends AppCompatActivity {
 
@@ -10,18 +11,19 @@ public class ActivitatSecundaria extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_activitat_secundaria);
-
+        //instanciem el objectes que rebran la informacio del bundle que volem mostrar
         TextView visualitzaNom=(TextView)findViewById(R.id.textView);
         TextView visualitzaCognom=(TextView)findViewById(R.id.textView6);
         TextView visualitzaEdat=(TextView)findViewById(R.id.textView7);
-        TextView visualitzaPublicacio=(TextView)findViewById(R.id.textView8);
-
-
+        //fem que el bundle posi la innfo
         Bundle tren = getIntent().getExtras();
-
+        //toast ni p idea como va (repasar)
+        Toast toast= Toast.makeText(getApplicationContext(),tren.getString("marcat"), Toast.LENGTH_LONG);
+        //fem que el objecte agafi el bundle que li volem assignar
         visualitzaNom.setText(tren.getString("Nom"));
         visualitzaCognom.setText(tren.getString("Cognom"));
         visualitzaEdat.setText(tren.getString("Edat"));
-        visualitzaPublicacio.setText(tren.getString("marcat"));
+        //per mostrar
+        toast.show();
     }
 }
